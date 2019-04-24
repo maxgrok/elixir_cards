66d51841b5fe912f52232f6cdd0104550beb5ed3
Max Goodman <max.goodman@flatironschool.com>
2019-04-24 17:32:36 -0400

    adding pipe operator

--- lib/cards.ex
+++ lib/cards.ex
@@ -28,8 +28,13 @@ end
  def load(filename) do 
   case File.read(filename) do 
     {:ok, binary} -> :erlang.binary_to_term binary
-    {:error, "That file does not exist"}
+    {:error, _reason} -> "That file does not exist"
   end
  end
 
+ def create_hand(hand_size) do 
+  Cards.create_deck 
+  |> Cards.shuffle 
+  |> Cards.deal(hand_size)
+ end
 end
